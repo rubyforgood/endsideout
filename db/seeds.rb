0728ebd7554545b5300dee736bc48dc25506c4e5
Sean Dickinson <sean@bythepixel.com>
2026-04-18 14:18:25 -0400

    feat: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+return unless Rails.env.development?
+
+User.find_or_create_by!(email_address: "admin@example.com") do |user|
+  user.name = "Admin"
+  user.password = "password"
+end
