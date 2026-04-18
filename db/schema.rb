e24ecd8febd66d3b2d419da9eba3c689c8ba747b
Sean Dickinson <sean@bythepixel.com>
2026-04-18 14:14:31 -0400

    feat: schools

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_18_174658) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_18_182356) do
+  create_table "schools", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
