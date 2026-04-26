b788afa996f8bef4c4e2852d301968db004fd941
Sean Dickinson <sean@bythepixel.com>
2026-04-26 13:26:35 -0400

    fix: remove local ci seed test

--- config/ci.rb
+++ config/ci.rb
@@ -9,7 +9,6 @@ CI.run do
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
   step "Tests: Rails", "bin/rails test"
-  step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: Run system tests
   # step "Tests: System", "bin/rails test:system"
