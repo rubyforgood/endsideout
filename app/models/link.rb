be4eda7531a8b1c1ddc7c91db95f77f8d656591c
Sean Dickinson <sean@bythepixel.com>
2026-05-17 14:23:07 -0400

    fix: brakeman warning

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,7 @@ class Link < ApplicationRecord
   enum :link_type, { survey: "survey", game: "game" }, validate: true
 
   validates :title, :url, :link_type, presence: true
+  validates :url, format: { with: /\Ahttps?:\/\/.+\z/i, message: "must start with http:// or https://" }, allow_blank: true
 
   default_scope { order(:position) }
 end
