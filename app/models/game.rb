9ae838b3c09645cf10dc56e992b9b84e9313590c
Adam Bachman <13002+abachman@users.noreply.github.com>
2026-08-29 12:15:04 -0400

    add game attempt model and relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,8 @@ class Game < ApplicationRecord
 
   has_many :game_attempts, dependent: :destroy
 
+  has_many :game_attempts, dependent: :destroy
+
   validates :title, :slug, presence: true
   validates :title, :slug, uniqueness: true
 end
