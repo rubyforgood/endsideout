004b03d6de431eeb060b8a6b5fb0ea9638e565cd
A-Mozeak <alex@astar.app>
2026-08-29 17:18:21 -0400

    Preserve Link ordering on ContentModule.

--- app/models/content_module.rb
+++ app/models/content_module.rb
@@ -2,10 +2,14 @@ class ContentModule < ApplicationRecord
   include Ordered
 
   belongs_to :program
-  has_many :links, -> { ordered }, dependent: :destroy
+  has_many :links, -> { ordered },
+    through: :contents,
+    source: :contentable,
+    source_type: 'Link'
+
   has_many :games, dependent: :nullify
   has_many :classroom_modules, dependent: :restrict_with_error
-  has_many :contentables, dependent: :destroy
+  has_many :contents, dependent: :destroy
 
   enum :level, { basic: "basic", moderate: "moderate", advanced: "advanced" }, validate: true
 
