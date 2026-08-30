7c30204932cbaa4f1519b6c563dc3c8548a4539c
Adam Bachman <13002+abachman@users.noreply.github.com>
2026-08-30 10:52:08 -0400

    fix rubocop errors

--- app/controllers/game_landing_controller.rb
+++ app/controllers/game_landing_controller.rb
@@ -7,7 +7,7 @@ class GameLandingController < ApplicationController
     @game = Game.find(params[:id])
     @game_attempt_token = GameAttempt.generate_token(student_id: current_student.id, game_id: @game.id)
 
-    logger.info("game attempt token: #{{game_attempt_token: @game_attempt_token, student_id: current_student.id, game_id: @game.id}}")
+    logger.info("game attempt token: #{{ game_attempt_token: @game_attempt_token, student_id: current_student.id, game_id: @game.id }}")
   end
 
   def embed
