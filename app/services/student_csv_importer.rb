dae4cdfeaed20a725c881a61d5e2afed152d443c
Ahmad Cheers <acheers1@gmail.com>
2026-08-30 15:23:21 -0400

    WIP Successfully imported test csv, extracted records, created and associated records. Needs cleanup and refactoring.

--- app/services/student_csv_importer.rb
+++ app/services/student_csv_importer.rb
@@ -22,10 +22,6 @@ class StudentCsvImporter
     @csv.each_with_index do |row, index|
       next if row.blank?
 
-      #Instantiate but don't persist Classroom record
-      #If record is invalid push error message into error messages hash
-      #If any classroom records are invalid raise error and return error messages hash to user
-      #Loop over classrooms array and save! classroom records if error_messages[:classrooms] is empty 
       classroom = Classroom.new(
         school_id: @school_id,
         name: row['Class Name'],
@@ -35,7 +31,7 @@ class StudentCsvImporter
       @classrooms << classroom
 
       
-      # Classrooms must exist before checking validity of student records 
+      # Student must be associated with a Classroom before checking validity of Student records
       student = Student.new(
         first_name: row['Student First Name'],
         last_name: row['Student Last Name'],
@@ -53,10 +49,16 @@ class StudentCsvImporter
       )
 
       @error_messages[:teachers][index] = teacher.errors.full_messages if teacher.invalid?
+      teacher.classrooms << classroom
       @teachers << teacher
-
     end
 
+    save_records
+
+
+  end
+
+  def save_records
     ActiveRecord::Base.transaction do
       if @error_messages[:classrooms].empty?
         @classrooms.each(&:save!)
@@ -92,9 +94,9 @@ class StudentCsvImporter
 
       classroom = Classroom.find_by!(school_id: @school_id, teacher_id: teacher.id, name: row['Class Name'])
       puts "Found Classroom: #{classroom.name}"
-    #Extract and apply uuid column when creating classroom
-    #Extract and apply program column when creating classroom
-      classroom.programs.create_or_find_by!(name: row['Program'], level: row['Program Level'])
+
+      program = Program.create_or_find_by!(name: row['Program'])
+      classroom.classroom_programs.create_or_find_by!(program: program, level: row['Program Level'])
       puts "Created Program: #{classroom.programs.first.name}"
     #Extract and apply program level column when creating classroom
       classroom.students.create_or_find_by!(first_name: row['Student First Name'], last_name: row['Student Last Name'], grade_level: row['Grade Level'], school_id: @school_id)
